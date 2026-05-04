0ccb5bfac9578796716c87dad2096ccfb0005c5a
zacharyolah-alt <zacharyolah@hotmail.com>
2026-05-04 18:32:20 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -317,7 +317,18 @@ corner_size: cornerSize,
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
-              
+      <select
+  value={moduleType}
+  onChange={(e) => {
+    setModuleType(e.target.value);
+    setCornerSize("");
+  }}
+>
+  <option>Straight</option>
+  <option>Inside Corner</option>
+  <option>Outside Corner</option>
+  <option>Bridge</option>
+</select>
               <label className="uploadBox">
                 {uploading
                   ? "Uploading..."
@@ -344,20 +355,7 @@ corner_size: cornerSize,
                 <option>Free-moN</option>
                 <option>Other</option>
               </select>
-<div>
-              <select
-  value={moduleType}
-  onChange={(e) => {
-    setModuleType(e.target.value);
-    setCornerSize("");
-  }}
->
-  <option>Straight</option>
-  <option>Inside Corner</option>
-  <option>Outside Corner</option>
-  <option>Bridge</option>
-</select>
-</div>
+
               {(moduleType === "Inside Corner" || moduleType === "Outside Corner") && (
   <div>
   <select
